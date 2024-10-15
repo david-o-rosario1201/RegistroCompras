bc9bfafb6412dbfc7c537ec5bd78292802f7e333
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-15 03:17:45 -0400

    Creación de CategoriaDao

---
+++ app/src/main/java/edu/ucne/registrocompras/data/local/dao/CategoriaDao.kt
@@ -0,0 +1,27 @@
+package edu.ucne.registrocompras.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrocompras.data.local.entities.CategoriaEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoriaDao {
+    @Upsert
+    suspend fun addCategoria(categoriaEntity: CategoriaEntity)
+
+    @Query("""
+        SELECT * FROM Categorias
+        WHERE categoriaId = :categoriaId
+        LIMIT 1
+    """)
+    suspend fun getCategoria(categoriaId: Int): CategoriaEntity?
+
+    @Delete
+    suspend fun deleteCategoria(categoriaEntity: CategoriaEntity)
+
+    @Query("SELECT * FROM Categorias")
+    fun getCategorias(): Flow<List<CategoriaEntity>>
+}
