8f2318f47a074d22ae8e74c1652856a0a74dada8
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-15 03:25:55 -0400

    Agregamos @AndroidEntryPoint encima de la clase MainActivity y creamos la función BuildNavigationItems para crear los datos que necesito para el menu desplegable

--- app/src/main/java/edu/ucne/registrocompras/MainActivity.kt
+++ app/src/main/java/edu/ucne/registrocompras/MainActivity.kt
@@ -6,42 +6,79 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.outlined.Face
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.List
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.registrocompras.ui.theme.RegistroComprasTheme
+import edu.ucne.registrocompras.ui.theme.util.NavigationItem
+import edu.ucne.registrocompras.ui.theme.util.Route
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RegistroComprasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun BuildNavigationItems(): List<NavigationItem>{
+    return listOf(
+        NavigationItem(
+            title = "Home",
+            selectedIcon = Icons.Filled.Home,
+            unselectedIcon = Icons.Outlined.Home,
+            route = Route.Home
+        ),
+        NavigationItem(
+            title = "Proveedores",
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person,
+            route = Route.Proveedores
+        ),
+        NavigationItem(
+            title = "Clientes",
+            selectedIcon = Icons.Filled.Face,
+            unselectedIcon = Icons.Outlined.Face,
+            route = Route.Clientes
+        ),
+        NavigationItem(
+            title = "Categorias",
+            selectedIcon = Icons.Filled.List,
+            unselectedIcon = Icons.Outlined.List,
+            route = Route.Categorias
+        ),
+        NavigationItem(
+            title = "Productos",
+            selectedIcon = Icons.Filled.Favorite,
+            unselectedIcon = Icons.Outlined.Favorite,
+            route = Route.Productos
+        ),
+        NavigationItem(
+            title = "Compras",
+            selectedIcon = Icons.Filled.ShoppingCart,
+            unselectedIcon = Icons.Outlined.ShoppingCart,
+            route = Route.Compras
+        )
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegistroComprasTheme {
-        Greeting("Android")
-    }
 }
