7499ab51748376ae7586784659a2fb122e05b256
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-15 03:23:28 -0400

    Creación de ProductoRepository

---
+++ app/src/main/java/edu/ucne/registrocompras/data/repository/ProductoRepository.kt
@@ -0,0 +1,34 @@
+package edu.ucne.registrocompras.data.repository
+
+import edu.ucne.registrocompras.data.remote.RemoteDataSource
+import edu.ucne.registrocompras.data.remote.Resource
+import edu.ucne.registrocompras.data.remote.dto.ProductoDto
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import javax.inject.Inject
+
+class ProductoRepository @Inject constructor(
+    private val remoteDataSource: RemoteDataSource
+) {
+    suspend fun addProducto(productoDto: ProductoDto) = remoteDataSource.addProducto(productoDto)
+
+    suspend fun getProducto(productoId: Int) = remoteDataSource.getProducto(productoId)
+
+    suspend fun deleteProducto(productoId: Int) = remoteDataSource.deleteProducto(productoId)
+
+    suspend fun updateProducto(productoId: Int) = remoteDataSource.updateProducto(productoId)
+
+    fun getProductos(): Flow<Resource<List<ProductoDto>>> = flow{
+        try {
+            emit(Resource.Loading())
+            val productos = remoteDataSource.getProductos()
+
+            emit(Resource.Success(productos))
+        } catch (e: HttpException){
+            emit(Resource.Error("Error de internet ${e.message}"))
+        } catch (e: Exception){
+            emit(Resource.Error("Error desconocido ${e.message}"))
+        }
+    }
+}
