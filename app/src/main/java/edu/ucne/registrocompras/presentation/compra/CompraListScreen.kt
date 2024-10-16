774dbf61d71ba0d6209da654062131a3811aa7b2
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 01:10:54 -0400

    Creación de CompraListScreen

---
+++ app/src/main/java/edu/ucne/registrocompras/presentation/compra/CompraListScreen.kt
@@ -0,0 +1,265 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+package edu.ucne.registrocompras.presentation.compra
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrocompras.R
+import edu.ucne.registrocompras.data.remote.dto.CompraDto
+import edu.ucne.registrocompras.data.remote.dto.ProductoDto
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+@Composable
+fun CompraListScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    viewModel: CompraViewModel = hiltViewModel(),
+    onClickCompra: (Int) -> Unit,
+    onAddCompra: () -> Unit
+){
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    CompraListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState = uiState,
+        onClickCompra = onClickCompra,
+        onAddCompra = onAddCompra
+    )
+}
+
+@Composable
+private fun CompraListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: CompraUiState,
+    onClickCompra: (Int) -> Unit,
+    onAddCompra: () -> Unit
+){
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Compras",
+                        style = MaterialTheme.typography.displaySmall,
+                        fontWeight = FontWeight.Bold
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            scope.launch {
+                                drawerState.open()
+                            }
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "Menu"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = onAddCompra
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Agregar Nueva Compra"
+                )
+            }
+        }
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+                .padding(
+                    start = 15.dp,
+                    end = 15.dp
+                )
+        ){
+            Spacer(modifier = Modifier.height(32.dp))
+
+            if(uiState.isLoading){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    CircularProgressIndicator()
+                }
+            }
+            else{
+                if(uiState.errorCargar?.isNotBlank() == true){
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ){
+                        uiState.errorCargar?.let {
+                            Text(
+                                text = it,
+                                color = Color.Red
+                            )
+                        }
+                    }
+                }
+                else{
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ){
+                        if(uiState.compras.isEmpty()){
+                            item {
+                                Column(
+                                    modifier = Modifier
+                                        .fillParentMaxSize(),
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    verticalArrangement = Arrangement.Center
+                                ){
+                                    Image(
+                                        painter = painterResource(R.drawable.empty_icon),
+                                        contentDescription = "Lista vacía"
+                                    )
+                                    Text(
+                                        text = "Lista vacía",
+                                        style = MaterialTheme.typography.bodyMedium,
+                                        fontWeight = FontWeight.Bold
+                                    )
+                                }
+                            }
+                        }else{
+                            items(uiState.compras){
+                                CompraRow(
+                                    it = it,
+                                    productos = uiState.productos,
+                                    onClickCompra = onClickCompra
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CompraRow(
+    it: CompraDto,
+    productos: List<ProductoDto>,
+    onClickCompra: (Int) -> Unit
+){
+    val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+
+    val descripcionProducto = productos.find { producto ->
+        producto.productoId == it.productoId
+    }?.nombre ?: ""
+
+    Card(
+        onClick = {
+            onClickCompra(it.compraId ?: 0)
+        },
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFB0BEC5)
+        ),
+        modifier = Modifier
+            .padding(top = 20.dp)
+            .fillMaxWidth()
+            .heightIn(min = 100.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.compras_image),
+                contentDescription = "Productos Image",
+                modifier = Modifier
+                    .padding(vertical = 20.dp)
+                    .size(96.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 8.dp)
+            ) {
+                Text(
+                    text = "Fecha de Creación: ${dateFormat.format(it.fechaCompra)}",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Text(
+                    text = "Producto: $descripcionProducto",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Text(
+                    text = "Cantidad: ${it.cantidad}",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Text(
+                    text = "Precio Unitario: ${it.precioUnitario}",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Text(
+                    text = "Total Compra: ${it.totalCompra}",
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }
+    }
+}
