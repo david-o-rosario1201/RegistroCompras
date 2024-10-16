b5c20d96ca859e035e339f264f7b9ad7c89c894c
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 17:48:13 -0400

    Cambiando el dato a la fecha para poder manejarse a través del test

--- app/src/main/java/edu/ucne/registrocompras/data/remote/dto/CategoriaDto.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/remote/dto/CategoriaDto.kt
@@ -1,10 +1,14 @@
 package edu.ucne.registrocompras.data.remote.dto
 
+import java.text.SimpleDateFormat
 import java.time.Instant
 import java.util.Date
+import java.util.Locale
 
 data class CategoriaDto(
     val categoriaId: Int?,
     val descripcion: String,
-    val fechaCreacion: Date = Date.from(Instant.now())
+    val fechaCreacion: String = SimpleDateFormat(
+        "yyyy-MM-dd'T'HH:mm:ss",
+        Locale.getDefault()).format(Date.from(Instant.now()))
 )
