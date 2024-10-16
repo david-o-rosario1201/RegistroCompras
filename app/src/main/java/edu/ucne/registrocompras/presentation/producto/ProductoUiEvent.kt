b1a880a2252f55940ccd8f95696f20d0a5bc6c4e
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 01:08:52 -0400

    Creación de ProductoUiEvent

---
+++ app/src/main/java/edu/ucne/registrocompras/presentation/producto/ProductoUiEvent.kt
@@ -0,0 +1,14 @@
+package edu.ucne.registrocompras.presentation.producto
+
+sealed interface ProductoUiEvent {
+    data class ProductoIdChange(val productoId: Int): ProductoUiEvent
+    data class NombreChange(val nombre: String): ProductoUiEvent
+    data class DescripcionChange(val descripcion: String): ProductoUiEvent
+    data class CategoriaIdChange(val categoriaId: Int): ProductoUiEvent
+    data class ProveedorIdChange(val proveedorId: Int): ProductoUiEvent
+    data class PrecioChange(val precio: Float): ProductoUiEvent
+    data class SelectedProducto(val productoId: Int): ProductoUiEvent
+    data object Save: ProductoUiEvent
+    data object Delete: ProductoUiEvent
+
+}
