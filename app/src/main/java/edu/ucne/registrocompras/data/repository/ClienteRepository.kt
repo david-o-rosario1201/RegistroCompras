cd8fa94f1f8f2c333cb971826802b9288c697c90
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 16:08:32 -0400

    Agregando la lógica para cargar los datos en room

--- app/src/main/java/edu/ucne/registrocompras/data/repository/ClienteRepository.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/repository/ClienteRepository.kt
@@ -1,5 +1,7 @@
 package edu.ucne.registrocompras.data.repository
 
+import edu.ucne.registrocompras.data.local.dao.ClienteDao
+import edu.ucne.registrocompras.data.local.entities.ClienteEntity
 import edu.ucne.registrocompras.data.remote.RemoteDataSource
 import edu.ucne.registrocompras.data.remote.Resource
 import edu.ucne.registrocompras.data.remote.dto.ClienteDto
@@ -10,6 +12,7 @@ import javax.inject.Inject
 
 class ClienteRepository @Inject constructor(
     private val remoteDataSource: RemoteDataSource,
+    private val clienteDao: ClienteDao
 ) {
     suspend fun addCliente(clienteDto: ClienteDto) = remoteDataSource.addCliente(clienteDto)
 
@@ -19,16 +22,38 @@ class ClienteRepository @Inject constructor(
 
     suspend fun updateCliente(clienteId: Int, clienteDto: ClienteDto) = remoteDataSource.updateCliente(clienteId, clienteDto)
 
-    fun getClientes(): Flow<Resource<List<ClienteDto>>> = flow{
+    fun getClientes(): Flow<Resource<List<ClienteEntity>>> = flow{
         try {
             emit(Resource.Loading())
             val clientes = remoteDataSource.getClientes()
 
-            emit(Resource.Success(clientes))
+            clientes.forEach {
+                clienteDao.addCliente(
+                    it.toClienteEntity()
+                )
+            }
+
+            clienteDao.getClientes().collect { clientesLocal ->
+                emit(Resource.Success(clientesLocal))
+            }
+
         } catch (e: HttpException){
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception){
             emit(Resource.Error("Error desconocido ${e.message}"))
+
+            clienteDao.getClientes().collect { clientesLocal ->
+                emit(Resource.Success(clientesLocal))
+            }
         }
     }
 }
+
+private fun ClienteDto.toClienteEntity() = ClienteEntity (
+    clienteId = clienteId,
+    nombre = nombre,
+    telefono = telefono,
+    direccion = direccion,
+    cedula = cedula,
+    fechaCreacion = fechaCreacion
+)
