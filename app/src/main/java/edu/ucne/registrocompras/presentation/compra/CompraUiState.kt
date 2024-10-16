5e984e26940f7750871de8c2f7484159f9e273e6
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 01:10:16 -0400

    Creación de CompraUiState

---
+++ app/src/main/java/edu/ucne/registrocompras/presentation/compra/CompraUiState.kt
@@ -0,0 +1,25 @@
+package edu.ucne.registrocompras.presentation.compra
+
+import edu.ucne.registrocompras.data.remote.Resource
+import edu.ucne.registrocompras.data.remote.dto.CompraDto
+import edu.ucne.registrocompras.data.remote.dto.ProductoDto
+import java.time.Instant
+import java.util.Date
+
+data class CompraUiState(
+    val compraId: Int? = null,
+    val fechaCompra: Date = Date.from(Instant.now()),
+    val productoId: Int? = 0,
+    val cantidad: Int? = 0,
+    val precioUnitario: Float? = 0.0f,
+    val totalCompra: Float? = 0.0f,
+    val compras: List<CompraDto> = emptyList(),
+    val productos: List<ProductoDto> = emptyList(),
+    val errorProductoId: String? = "",
+    val errorCantidad: String? = "",
+    val errorPrecioUnitario: String? = "",
+    val errorTotalCompra: String? = "",
+    val errorCargar: String? = "",
+    val success: Boolean = false,
+    val isLoading: Boolean = false
+)
