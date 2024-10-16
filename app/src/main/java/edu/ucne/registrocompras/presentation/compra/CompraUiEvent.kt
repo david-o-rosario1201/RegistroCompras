d692ce488df51c4d9e7140549707d8899d6f7c31
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 12:05:02 -0400

    Cambiando cantidad y precioUnitario a String

--- app/src/main/java/edu/ucne/registrocompras/presentation/compra/CompraUiEvent.kt
+++ app/src/main/java/edu/ucne/registrocompras/presentation/compra/CompraUiEvent.kt
@@ -3,8 +3,8 @@ package edu.ucne.registrocompras.presentation.compra
 sealed interface CompraUiEvent {
     data class CompraIdChanged(val compraId: Int): CompraUiEvent
     data class ProductoIdChanged(val productoId: Int): CompraUiEvent
-    data class CantidadChanged(val cantidad: Int): CompraUiEvent
-    data class PrecioUnitarioChanged(val precioUnitario: Float): CompraUiEvent
+    data class CantidadChanged(val cantidad: String): CompraUiEvent
+    data class PrecioUnitarioChanged(val precioUnitario: String): CompraUiEvent
     data class SelectedCompra(val compraId: Int): CompraUiEvent
     data object Save: CompraUiEvent
     data object Delete: CompraUiEvent
