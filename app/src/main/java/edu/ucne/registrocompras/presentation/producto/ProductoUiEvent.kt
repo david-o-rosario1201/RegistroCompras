88113a532050baaf715a44e8aad0075fa4303a76
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 12:01:45 -0400

    Cambiando precio a String

--- app/src/main/java/edu/ucne/registrocompras/presentation/producto/ProductoUiEvent.kt
+++ app/src/main/java/edu/ucne/registrocompras/presentation/producto/ProductoUiEvent.kt
@@ -6,7 +6,7 @@ sealed interface ProductoUiEvent {
     data class DescripcionChange(val descripcion: String): ProductoUiEvent
     data class CategoriaIdChange(val categoriaId: Int): ProductoUiEvent
     data class ProveedorIdChange(val proveedorId: Int): ProductoUiEvent
-    data class PrecioChange(val precio: Float): ProductoUiEvent
+    data class PrecioChange(val precio: String): ProductoUiEvent
     data class SelectedProducto(val productoId: Int): ProductoUiEvent
     data object Save: ProductoUiEvent
     data object Delete: ProductoUiEvent
