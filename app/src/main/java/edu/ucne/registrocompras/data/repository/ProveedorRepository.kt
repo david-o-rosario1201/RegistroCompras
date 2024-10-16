2d4861dced70813e0e58945a13a07924554cd8a4
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 16:05:28 -0400

    Agregando la lógica para cargue los datos en room

--- app/src/main/java/edu/ucne/registrocompras/data/repository/ProveedorRepository.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/repository/ProveedorRepository.kt
@@ -1,5 +1,7 @@
 package edu.ucne.registrocompras.data.repository
 
+import edu.ucne.registrocompras.data.local.dao.ProveedorDao
+import edu.ucne.registrocompras.data.local.entities.ProveedorEntity
 import edu.ucne.registrocompras.data.remote.RemoteDataSource
 import edu.ucne.registrocompras.data.remote.Resource
 import edu.ucne.registrocompras.data.remote.dto.ProveedorDto
@@ -9,7 +11,8 @@ import retrofit2.HttpException
 import javax.inject.Inject
 
 class ProveedorRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val proveedorDao: ProveedorDao
 ) {
     suspend fun addProveedor(proveedorDto: ProveedorDto) = remoteDataSource.addProveedor(proveedorDto)
 
@@ -19,16 +22,37 @@ class ProveedorRepository @Inject constructor(
 
     suspend fun updateProveedor(proveedorId: Int, proveedorDto: ProveedorDto) = remoteDataSource.updateProveedor(proveedorId, proveedorDto)
 
-    fun getProveedores(): Flow<Resource<List<ProveedorDto>>> = flow{
+    fun getProveedores(): Flow<Resource<List<ProveedorEntity>>> = flow{
         try {
             emit(Resource.Loading())
             val proveedores = remoteDataSource.getProveedores()
 
-            emit(Resource.Success(proveedores))
+            proveedores.forEach {
+                proveedorDao.addProveedor(
+                    it.toProveedorEntity()
+                )
+            }
+
+            proveedorDao.getProveedores().collect { proveedoresLocal ->
+                emit(Resource.Success(proveedoresLocal))
+            }
         } catch (e: HttpException){
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception){
             emit(Resource.Error("Error desconocido ${e.message}"))
+
+            proveedorDao.getProveedores().collect { proveedoresLocal ->
+                emit(Resource.Success(proveedoresLocal))
+            }
         }
     }
 }
+
+private fun ProveedorDto.toProveedorEntity() = ProveedorEntity (
+    proveedorId = proveedorId,
+    nombre = nombre,
+    rnc = rnc,
+    direccion = direccion,
+    email = email,
+    fechaCreacion = fechaCreacion
+)
