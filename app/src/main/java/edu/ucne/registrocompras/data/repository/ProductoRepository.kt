93845c52151349cf599e39b52578e7f78b490d0a
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 16:13:41 -0400

    Agregando la lógica para cargar los datos en room

--- app/src/main/java/edu/ucne/registrocompras/data/repository/ProductoRepository.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/repository/ProductoRepository.kt
@@ -1,5 +1,7 @@
 package edu.ucne.registrocompras.data.repository
 
+import edu.ucne.registrocompras.data.local.dao.ProductoDao
+import edu.ucne.registrocompras.data.local.entities.ProductoEntity
 import edu.ucne.registrocompras.data.remote.RemoteDataSource
 import edu.ucne.registrocompras.data.remote.Resource
 import edu.ucne.registrocompras.data.remote.dto.ProductoDto
@@ -9,7 +11,8 @@ import retrofit2.HttpException
 import javax.inject.Inject
 
 class ProductoRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val productoDao: ProductoDao
 ) {
     suspend fun addProducto(productoDto: ProductoDto) = remoteDataSource.addProducto(productoDto)
 
@@ -19,16 +22,39 @@ class ProductoRepository @Inject constructor(
 
     suspend fun updateProducto(productoId: Int, productoDto: ProductoDto) = remoteDataSource.updateProducto(productoId, productoDto)
 
-    fun getProductos(): Flow<Resource<List<ProductoDto>>> = flow{
+    fun getProductos(): Flow<Resource<List<ProductoEntity>>> = flow{
         try {
             emit(Resource.Loading())
             val productos = remoteDataSource.getProductos()
 
-            emit(Resource.Success(productos))
+            productos.forEach {
+                productoDao.addProducto(
+                    it.toProductoEntity()
+                )
+            }
+
+            productoDao.getProductos().collect { productosLocal ->
+                emit(Resource.Success(productosLocal))
+            }
+
         } catch (e: HttpException){
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception){
             emit(Resource.Error("Error desconocido ${e.message}"))
+
+            productoDao.getProductos().collect { productosLocal ->
+                emit(Resource.Success(productosLocal))
+            }
         }
     }
 }
+
+private fun ProductoDto.toProductoEntity() = ProductoEntity (
+    productoId = productoId,
+    nombre = nombre,
+    precio = precio,
+    categoriaId = categoriaId,
+    proveedorId = proveedorId,
+    fechaCreacion = fechaCreacion,
+    descripcion = descripcion
+)
