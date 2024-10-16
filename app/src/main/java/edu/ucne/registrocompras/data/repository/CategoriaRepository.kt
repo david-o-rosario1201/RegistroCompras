66fec7574ee9350756f3faa99d2e41234e3befc8
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 16:10:33 -0400

    Agregando la lógica para cargar los datos en room

--- app/src/main/java/edu/ucne/registrocompras/data/repository/CategoriaRepository.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/repository/CategoriaRepository.kt
@@ -1,5 +1,7 @@
 package edu.ucne.registrocompras.data.repository
 
+import edu.ucne.registrocompras.data.local.dao.CategoriaDao
+import edu.ucne.registrocompras.data.local.entities.CategoriaEntity
 import edu.ucne.registrocompras.data.remote.RemoteDataSource
 import edu.ucne.registrocompras.data.remote.Resource
 import edu.ucne.registrocompras.data.remote.dto.CategoriaDto
@@ -9,7 +11,8 @@ import retrofit2.HttpException
 import javax.inject.Inject
 
 class CategoriaRepository @Inject constructor(
-    private val remoteDataSource: RemoteDataSource
+    private val remoteDataSource: RemoteDataSource,
+    private val categoriaDao: CategoriaDao
 ){
     suspend fun addCategoria(categoriaDto: CategoriaDto) = remoteDataSource.addCategoria(categoriaDto)
 
@@ -19,16 +22,35 @@ class CategoriaRepository @Inject constructor(
 
     suspend fun updateCategoria(categoriaId: Int, categoriaDto: CategoriaDto) = remoteDataSource.updateCategoria(categoriaId, categoriaDto)
 
-    fun getCategorias(): Flow<Resource<List<CategoriaDto>>> = flow{
+    fun getCategorias(): Flow<Resource<List<CategoriaEntity>>> = flow{
         try {
             emit(Resource.Loading())
             val categorias = remoteDataSource.getCategorias()
 
-            emit(Resource.Success(categorias))
+            categorias.forEach {
+                categoriaDao.addCategoria(
+                    it.toCategoriaEntity()
+                )
+            }
+
+            categoriaDao.getCategorias().collect { categoriasLocal ->
+                emit(Resource.Success(categoriasLocal))
+            }
+
         } catch (e: HttpException){
             emit(Resource.Error("Error de internet ${e.message}"))
         } catch (e: Exception){
             emit(Resource.Error("Error desconocido ${e.message}"))
+
+            categoriaDao.getCategorias().collect { categoriasLocal ->
+                emit(Resource.Success(categoriasLocal))
+            }
         }
     }
 }
+
+private fun CategoriaDto.toCategoriaEntity() = CategoriaEntity (
+    categoriaId = categoriaId,
+    descripcion = descripcion,
+    fechaCreacion = fechaCreacion
+)
