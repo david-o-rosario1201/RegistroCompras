42c8db7f08bd61f8aeb036029874bdf953e6ac94
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 01:01:15 -0400

    Se agregaron las demás entidades a la base de datos y se agregó el Converter para manejar las fechas en la base de datos.

--- app/src/main/java/edu/ucne/registrocompras/data/local/database/ComprasDb.kt
+++ app/src/main/java/edu/ucne/registrocompras/data/local/database/ComprasDb.kt
@@ -2,22 +2,34 @@ package edu.ucne.registrocompras.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import edu.ucne.registrocompras.data.local.dao.CategoriaDao
 import edu.ucne.registrocompras.data.local.dao.ClienteDao
 import edu.ucne.registrocompras.data.local.dao.CompraDao
 import edu.ucne.registrocompras.data.local.dao.ProductoDao
 import edu.ucne.registrocompras.data.local.dao.ProveedorDao
+import edu.ucne.registrocompras.data.local.entities.CategoriaEntity
+import edu.ucne.registrocompras.data.local.entities.ClienteEntity
+import edu.ucne.registrocompras.data.local.entities.CompraEntity
+import edu.ucne.registrocompras.data.local.entities.ProductoEntity
 import edu.ucne.registrocompras.data.local.entities.ProveedorEntity
 
 @Database(
-    entities = [ProveedorEntity::class],
+    entities = [
+        ProveedorEntity::class,
+        ClienteEntity::class,
+        CategoriaEntity::class,
+        ProductoEntity::class,
+        CompraEntity::class
+    ],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class ComprasDb: RoomDatabase(){
     abstract fun proveedorDao(): ProveedorDao
     abstract fun clienteDao(): ClienteDao
     abstract fun categoriaDao(): CategoriaDao
     abstract fun productoDao(): ProductoDao
     abstract fun compraDao(): CompraDao
-}
+}   
