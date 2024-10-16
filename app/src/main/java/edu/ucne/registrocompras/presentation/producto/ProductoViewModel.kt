6c004761fcf60f3de0dc09d3e249895536b8da12
david-o-rosario1201 <finnnn023@gmail.com>
2024-10-16 12:02:53 -0400

    Agregando lógica para eliminar y guardar productos

--- app/src/main/java/edu/ucne/registrocompras/presentation/producto/ProductoViewModel.kt
+++ app/src/main/java/edu/ucne/registrocompras/presentation/producto/ProductoViewModel.kt
@@ -139,35 +139,40 @@ class ProductoViewModel @Inject constructor(
             is ProductoUiEvent.NombreChange -> {
                 _uiState.update {
                     it.copy(
-                        nombre = event.nombre
+                        nombre = event.nombre,
+                        errorNombre = ""
                     )
                 }
             }
             is ProductoUiEvent.DescripcionChange -> {
                 _uiState.update {
                     it.copy(
-                        descripcion = event.descripcion
+                        descripcion = event.descripcion,
+                        errorDescripcion = ""
                     )
                 }
             }
             is ProductoUiEvent.CategoriaIdChange -> {
                 _uiState.update {
                     it.copy(
-                        categoriaId = event.categoriaId
+                        categoriaId = event.categoriaId,
+                        errorCategoriaId = ""
                     )
                 }
             }
             is ProductoUiEvent.ProveedorIdChange -> {
                 _uiState.update {
                     it.copy(
-                        proveedorId = event.proveedorId
+                        proveedorId = event.proveedorId,
+                        errorProveedorId = ""
                     )
                 }
             }
             is ProductoUiEvent.PrecioChange -> {
                 _uiState.update {
                     it.copy(
-                        precio = event.precio
+                        precio = event.precio,
+                        errorPrecio = ""
                     )
                 }
             }
@@ -183,14 +188,107 @@ class ProductoViewModel @Inject constructor(
                                 fechaCreacion = producto.fechaCreacion,
                                 categoriaId = producto.categoriaId,
                                 proveedorId = producto.proveedorId,
-                                precio = producto.precio
+                                precio = producto.precio.toString()
                             )
                         }
                     }
                 }
             }
-            ProductoUiEvent.Save -> TODO()
-            ProductoUiEvent.Delete -> TODO()
+            ProductoUiEvent.Save -> {
+                viewModelScope.launch {
+
+                    val nombreBuscado = _uiState.value.productos
+                        .find { it.nombre.lowercase() == _uiState.value.nombre?.lowercase() }
+
+                    val descripcionBuscada = _uiState.value.productos
+                        .find { it.descripcion.lowercase() == _uiState.value.descripcion?.lowercase() }
+
+                    if(_uiState.value.nombre?.isBlank() == true){
+                        _uiState.update {
+                            it.copy(
+                                errorNombre = "El nombre no puede estar vacío"
+                            )
+                        }
+                    }
+                    else if(nombreBuscado != null && nombreBuscado.productoId != _uiState.value.productoId){
+                        _uiState.update {
+                            it.copy(
+                                errorNombre = "El nombre ya existe"
+                            )
+                        }
+                    }
+
+                    if(_uiState.value.descripcion?.isBlank() == true) {
+                        _uiState.update {
+                            it.copy(
+                                errorDescripcion = "La descripción no puede estar vacío"
+                            )
+                        }
+                    }
+                    else if(descripcionBuscada != null && descripcionBuscada.productoId != _uiState.value.productoId) {
+                        _uiState.update {
+                            it.copy(
+                                errorDescripcion = "La descripción ya existe"
+                            )
+                        }
+                    }
+
+                    if(_uiState.value.categoriaId == 0) {
+                        _uiState.update {
+                            it.copy(
+                                errorCategoriaId = "Debe seleccionar una categoría"
+                            )
+                        }
+                    }
+
+                    if(_uiState.value.proveedorId == 0) {
+                        _uiState.update {
+                            it.copy(
+                                errorProveedorId = "Debe seleccionar un proveedor"
+                            )
+                        }
+                    }
+
+                    if(_uiState.value.precio?.isBlank() == true) {
+                        _uiState.update {
+                            it.copy(
+                                errorPrecio = "El precio no puede estar vacío"
+                            )
+                        }
+                    }
+                    else if(_uiState.value.precio?.toFloat()!! < 0.0f) {
+                        _uiState.update {
+                            it.copy(
+                                errorPrecio = "El precio no puede ser negativo"
+                            )
+                        }
+                    }
+
+                    if(_uiState.value.errorNombre == "" && _uiState.value.errorDescripcion == "" &&
+                        _uiState.value.errorCategoriaId == "" && _uiState.value.errorProveedorId == ""
+                        && _uiState.value.errorPrecio == ""){
+
+                        if(_uiState.value.productoId == null)
+                            productoRepository.addProducto(_uiState.value.toEntity())
+                        else
+                            productoRepository.updateProducto(
+                                _uiState.value.productoId ?: 0,
+                                _uiState.value.toEntity()
+                            )
+
+                        _uiState.update {
+                            it.copy(
+                                success = true
+                            )
+                        }
+                    }
+                }
+            }
+            ProductoUiEvent.Delete -> {
+                viewModelScope.launch {
+                    productoRepository.deleteProducto(_uiState.value.productoId ?: 0)
+                }
+            }
         }
     }
 
@@ -201,6 +299,6 @@ class ProductoViewModel @Inject constructor(
         fechaCreacion = fechaCreacion,
         categoriaId = categoriaId ?: 0,
         proveedorId = proveedorId ?: 0,
-        precio = precio ?: 0.0f
+        precio = precio.toString().toFloatOrNull() ?: 0.0f
     )
 }
